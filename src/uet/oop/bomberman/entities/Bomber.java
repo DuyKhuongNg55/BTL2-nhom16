010dde28a8ee5d7e059c5df08deb94d41706e8b1
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2021-12-04 18:30:37 +0700

    Sửa lỗi Chiến bảo

--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -144,7 +144,7 @@ public class Bomber extends Entity {
       }
 
       for(int i = 0 ; i < BombermanGame.getBombList().size();i++){
-        if((this.getX() + speed) / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getX() / Sprite.SCALED_SIZE &&
+        if((this.getX() + speed + 20) / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getX() / Sprite.SCALED_SIZE &&
                 this.getY()  / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getY() / Sprite.SCALED_SIZE){
           if(!BombermanGame.getBombList().get(i)._allowedToPassThru) return;
         }
@@ -198,7 +198,7 @@ public class Bomber extends Entity {
       }
 
       for(int i = 0 ; i < BombermanGame.getBombList().size();i++){
-        if((this.getX() - speed) / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getX() / Sprite.SCALED_SIZE &&
+        if((this.getX() - speed - 20) / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getX() / Sprite.SCALED_SIZE &&
                 this.getY()  / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getY() / Sprite.SCALED_SIZE){
           if(!BombermanGame.getBombList().get(i)._allowedToPassThru) return;
         }
@@ -250,7 +250,7 @@ public class Bomber extends Entity {
 
       for(int i = 0 ; i < BombermanGame.getBombList().size();i++){
         if(this.getX() / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getX() / Sprite.SCALED_SIZE &&
-                (this.getY() - speed) / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getY() / Sprite.SCALED_SIZE){
+                (this.getY() - speed-20) / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getY() / Sprite.SCALED_SIZE){
           if(!BombermanGame.getBombList().get(i)._allowedToPassThru) return;
         }
       }
@@ -301,7 +301,7 @@ public class Bomber extends Entity {
 
       for(int i = 0 ; i < BombermanGame.getBombList().size();i++){
         if(this.getX() / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getX() / Sprite.SCALED_SIZE &&
-                (this.getY() + speed) / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getY() / Sprite.SCALED_SIZE){
+                (this.getY() + speed+20) / Sprite.SCALED_SIZE == BombermanGame.getBombList().get(i).getY() / Sprite.SCALED_SIZE){
           if(!BombermanGame.getBombList().get(i)._allowedToPassThru) return;
         }
       }
